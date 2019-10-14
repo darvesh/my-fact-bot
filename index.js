6bec7a48cad05f36519c5183bbfb1f36d1c647b2
Fakruddin Darvesh A.K <darvesh696@yahoo.com>
2019-10-15 02:20:22 +0530

    adds start message

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const fetcher = require("./fetcher");
 
 const bot = new Telegraf(config.BOT_TOKEN);
 
+bot.start(ctx =>
+	ctx.replyWithHTML(`
+<b>Commands Available:</b>
+<code>
+/math &lt;number&gt;
+/date &lt;month&gt; &lt;day&gt;
+/year &lt;year&gt;
+/trivia &lt;number&gt;
+</code>
+To get random reply, just send /command without any additional message.
+
+Source code: https://github.com/solooo7/number-fact-bot
+`)
+);
 bot.command("math", ctx => fetcher.math(axios, ctx));
 bot.command("date", ctx => fetcher.date(axios, ctx));
 bot.command("year", ctx => fetcher.year(axios, ctx));
